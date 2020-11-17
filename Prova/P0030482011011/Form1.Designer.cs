1636e202b9f78e982b8a9b988e1f62958c467f0c
Gio991 <69857234+Gio991@users.noreply.github.com>
2020-11-17 11:20:40 -0300

    Add files via upload

---
+++ Prova/P0030482011011/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Prova_I
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bttn1 = new System.Windows.Forms.Button();
+            this.lstbxSaidas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // bttn1
+            // 
+            this.bttn1.Location = new System.Drawing.Point(139, 156);
+            this.bttn1.Name = "bttn1";
+            this.bttn1.Size = new System.Drawing.Size(173, 147);
+            this.bttn1.TabIndex = 0;
+            this.bttn1.Text = "Iniciar!";
+            this.bttn1.UseVisualStyleBackColor = true;
+            this.bttn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstbxSaidas
+            // 
+            this.lstbxSaidas.FormattingEnabled = true;
+            this.lstbxSaidas.Location = new System.Drawing.Point(441, 12);
+            this.lstbxSaidas.Name = "lstbxSaidas";
+            this.lstbxSaidas.Size = new System.Drawing.Size(321, 407);
+            this.lstbxSaidas.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstbxSaidas);
+            this.Controls.Add(this.bttn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bttn1;
+        private System.Windows.Forms.ListBox lstbxSaidas;
+    }
+}
+
---
+++ Prova/P0030482011011/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Prova_I
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double[,] arrayVendas = new double[1, 4];
+        double totSemana, totMes, totGeral;
+        string valor;
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+
+                    valor = Interaction.InputBox("Entrada de dados", "Digite o valor de vendas.");
+
+                    double vendas = double.Parse(valor);
+
+                    totSemana = vendas;
+                    totMes = totMes + totSemana;
+                    
+
+                    lstbxSaidas.Items.Add($" Total da semana {j + 1}: {totSemana:c2}");
+
+                }
+                totGeral = totGeral + totMes;
+
+                lstbxSaidas.Items.Add($" Total do mês: {totMes:c2}");
+                lstbxSaidas.Items.Add($" Total geral: {totGeral:c2}");
+
+            }
+        }
+    }
+}
